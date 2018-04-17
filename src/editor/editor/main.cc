07b34680acabd087d9d75440796330d47a479298
Riko Ophorst <riko_ophorst@hotmail.com>
2018-04-17 22:43:18 +0200

    Rework the editor startup so that it runs in parallel with the new blowbox::engine::Engine.

--- src/editor/editor/main.cc
+++ src/editor/editor/main.cc
@@ -1,25 +1,52 @@
 #include "editor/main_window.h"
 
 #include "core/logger.h"
+#include "engine/engine.h"
 
 #include <QtWidgets/qapplication.h>
 
 using namespace blowbox;
 
+struct AppData
+{
+  AppData(int argc, char** argv) : app(argc, argv) {}
+  QApplication app;
+};
+
+void OnInitialize(void* userdata)
+{
+  AppData* app_data = reinterpret_cast<AppData*>(userdata);
+
+  editor::MainWindow* window = new editor::MainWindow(&app_data->app);
+  window->show();
+}
+
+void OnUpdate(void* userdata)
+{
+  core::Logger::Log(core::MessageSource::Editor, "This is a log");
+  core::Logger::Warn(core::MessageSource::Editor, "This is a warning");
+  core::Logger::Error(core::MessageSource::Editor, "This is a error");
+}
+
+void OnEndFrame(void* userdata)
+{
+  AppData* app_data = reinterpret_cast<AppData*>(userdata);
+
+  app_data->app.processEvents();
+}
+
 int main(int argc, char** argv)
 {
-  QApplication app(argc, argv);
+  AppData app_data(argc, argv);
 
-  editor::MainWindow* window = new editor::MainWindow(&app);
+  engine::Engine blowbox_editor(argc, argv);
+  
+  blowbox_editor.SetCallbackUserdata(&app_data);
+  blowbox_editor.SetOnInitialize(OnInitialize);
+  blowbox_editor.SetOnUpdate(OnUpdate);
+  blowbox_editor.SetOnEndFrame(OnEndFrame);
+  
+  blowbox_editor.Run();
 
-  for (int i = 0; i < 100; i++)
-  {
-    core::Logger::Log(core::MessageSource::Editor, "This is a log");
-    core::Logger::Warn(core::MessageSource::Editor, "This is a warning");
-    core::Logger::Error(core::MessageSource::Editor, "This is a error");
-  }
-
-  window->show();
-
-  return app.exec();
+  return 0;
 }
