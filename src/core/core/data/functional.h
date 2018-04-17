f92e046e26c993c0330639fbd3d8a61e3d269b78
Riko Ophorst <riko_ophorst@hotmail.com>
2018-04-17 22:41:01 +0200

    Add a safe execution method for invoking eastl::functions.

--- src/core/core/data/functional.h
+++ src/core/core/data/functional.h
@@ -1,3 +1,18 @@
 #pragma once
 
 #include <EASTL/functional.h>
+
+#define BLOWBOX_CALL_FUNCTION_SAFELY(func, ...) if (func) { func(##__VA_ARGS__); }
+
+namespace blowbox
+{
+  //------------------------------------------------------------------------------------------------------
+  template<typename T, typename ... Args>
+  inline void ExecuteCallbackSafely(const T& callback, Args&&... arguments)
+  {
+    if (callback)
+    {
+      callback(arguments...);
+    }
+  }
+}
